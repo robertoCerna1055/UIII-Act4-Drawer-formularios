2387b0d8264a6c648981812ccaa5ee638fa7eb31
a.22308051281055 <a.22308051281055@cbtis128.edu.mx>
2025-05-29 15:52:59 +0000

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        elevation: 0.2,
+        title: Text("Formularios tablas"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+        ],
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.pink),
+              accountName: Text("Roberto Cerna 1055"), 
+              accountEmail: Text("roberto.cer@cbtis128.edu.mx"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    "AM",
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ recursos/leeme.md
@@ -0,0 +1,5 @@
+## AppBar + Drawer
+![alt text](image.png)
+
+## Encabezado drawer
+![alt text](image-1.png)
